d2896cf399dd10afcf4439a56a56629231d86576
Michael Clifford <cliffom@gmail.com>
2017-12-06 13:09:36 -0800

    map_public_ip_on_launch = true for subnets

--- vpc.tf
+++ vpc.tf
@@ -5,14 +5,14 @@ resource "aws_vpc" "terraws" {
   enable_dns_hostnames = true
 }
 
-resource "aws_internet_gateway" "terraws-ig" {
+resource "aws_internet_gateway" "terraws" {
   vpc_id = "${aws_vpc.terraws.id}"
 }
 
 resource "aws_route" "default" {
   route_table_id          = "${aws_vpc.terraws.main_route_table_id}"
   destination_cidr_block  = "0.0.0.0/0"
-  gateway_id              = "${aws_internet_gateway.terraws-ig.id}"
+  gateway_id              = "${aws_internet_gateway.terraws.id}"
 }
 
 # Create subnets for load balancers
@@ -20,18 +20,21 @@ resource "aws_subnet" "elb-us-east-1a" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "elb-us-east-1b" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "elb-us-east-1c" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
 }
 
 # Create subnets for web instances
@@ -39,16 +42,19 @@ resource "aws_subnet" "web-us-east-1a" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.10.0/24"
   availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "web-us-east-1b" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.11.0/24"
   availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "web-us-east-1c" {
   vpc_id                  = "${aws_vpc.terraws.id}"
   cidr_block              = "10.0.12.0/24"
   availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
 }
