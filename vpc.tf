b3ae17435e121cb62e19b761d99ae2d8296ac282
Michael Clifford <cliffom@gmail.com>
2017-12-06 13:17:44 -0800

    Formatting

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "terraws" {
-  cidr_block = "10.0.0.0/16"
-
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  cidr_block            = "10.0.0.0/16"
+  enable_dns_support    = true
+  enable_dns_hostnames  = true
 }
 
 resource "aws_internet_gateway" "terraws" {
